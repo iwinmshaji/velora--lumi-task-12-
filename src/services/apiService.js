ef820c22c648b54228b9c9748e4c9bbcc4f1d421
Iwin Mshaji <iwinmshaji@users.noreply.github.com>
2026-08-18 18:23:29 +0530

    connect frontend to render api

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const API_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:3000";
+  import.meta.env.VITE_API_URL || "https://velora-api-neqj.onrender.com";
 
 const api = axios.create({
   baseURL: API_URL,
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 // PRODUCTS
